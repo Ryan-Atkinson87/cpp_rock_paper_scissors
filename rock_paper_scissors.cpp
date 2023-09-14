4cad8af57c49ed65e47338a9ba145e72ee8d47a2
Ryan Atkinson <64192853+Ryan-Atkinson87@users.noreply.github.com>
2023-09-14 18:54:44 +0100

    Corrected logic in exit loop, add string input for choosing to play

--- rock_paper_scissors.cpp
+++ rock_paper_scissors.cpp
@@ -17,52 +17,57 @@ int main() {
 
     
     bool exit = false;
-    bool play = true;
+    bool valid = false;
     int user = 0;
+    std::string input_string = "";
 
     //Names the game
     std::cout << "====================\n";
     std::cout << "rock paper scissors!\n";
     std::cout << "====================\n";
-
-    std::cout << "\n";
-    std::cout << "Would you like to play?\n";
-    std::cout << "\n";
-    std::cout << "0) Yes\n";
-    std::cout << "9) No\n";
-    std::cout << "Response: ";
-    std::cin >> user;
-    if (user == 9) {
-        exit = true;
+    while (valid == false) {
         std::cout << "\n";
-        std::cout << "Goodbye!!\n";
-    }
-    else {
+        std::cout << "Would you like to play?\n";
         std::cout << "\n";
-        std::cout << " Please enter a valid input!\n";
+        std::cout << "Response: ";
+        std::cin >> input_string;
+        std::cout << "\n";
+        if (input_string == "no" || input_string == "No") {
+            exit = true;
+            std::cout << "\n";
+            std::cout << "Goodbye!!\n";
+            valid = true;
+        }
+        else if (input_string == "yes" || input_string == "Yes") {
+            exit = false;
+            valid = true;
+        }
+        else {
+            std::cout << "\n";
+            std::cout << " Please enter yes or no!\n";
+        }
     }
-
     while (exit == false) {
+        valid = false;
+        while (valid == false) {
+            //Produces a random number everytime
+            computer = rand() % 3 + 1;
 
-        //Produces a random number everytime
-        computer = rand() % 3 + 1;
-
-        //check exit is still false
-        
-        //ask user for input
-        std::cout << "1) ✊\n";
-        std::cout << "2) ✋\n";
-        std::cout << "3) ✌️\n";
-        std::cout << "\n";
-        std::cout << "Press 9 to quit\n";
-        std::cout << "shoot! ";
-        std::cin >> user;
+            
+            //ask user for input
+            std::cout << "1) ✊\n";
+            std::cout << "2) ✋\n";
+            std::cout << "3) ✌️\n";
+            std::cout << "\n";
+            std::cout << "shoot! ";
+            std::cin >> user;
             if (user == 1 && computer == 2) {
                 std::cout << "\n";
                 std::cout << "You chose rock!\n";
                 std::cout << "Computer chose paper!\n";
                 std::cout << "\n";
                 std::cout << "Computer wins!\n";
+                valid = true;
             } 
             else if (user == 1 && computer == 3) {
                 std::cout << "\n";
@@ -70,6 +75,7 @@ int main() {
                 std::cout << "Computer chose scissors!\n";
                 std::cout << "\n";
                 std::cout << "You won!\n";
+                valid = true;
             }
             else if (user == 2 && computer == 1) {
                 std::cout << "\n";
@@ -77,13 +83,15 @@ int main() {
                 std::cout << "Computer chose rock!\n";
                 std::cout << "\n";
                 std::cout << "You won!\n";
+                valid = true;
             }
             else if (user == 2 && computer == 3) {
                 std::cout << "\n";
                 std::cout << "You chose paper!\n";
-                std::cout << "Computer chose scissor!\n";
+                std::cout << "Computer chose scissors!\n";
                 std::cout << "\n";
                 std::cout << "Computer wins!\n";
+                valid = true;
             }
             else if (user == 3 && computer == 1) {
                 std::cout << "\n";
@@ -91,6 +99,7 @@ int main() {
                 std::cout << "Computer chose rock!\n";
                 std::cout << "\n";
                 std::cout << "Computer wins!\n";
+                valid = true;
             }
             else if (user == 3 && computer == 2) {
                 std::cout << "\n";
@@ -98,35 +107,42 @@ int main() {
                 std::cout << "Computer chose paper!\n";
                 std::cout << "\n";
                 std::cout << "You won!\n";
+                valid = true;
             }
             else if (user == computer) {
                 std::cout << "\n";
                 std::cout << "You both chose the same!\n";
                 std::cout << "\n";
                 std::cout << "It's a draw!\n";
-            }
-            else if (user == 9) {
-                exit = true;
+                valid = true;
             }
             else {
-                std::cout << "Invalid input, press 1, 2, 3 or 9.\n";
+                std::cout << "Invalid input, press 1, 2 or 3.\n";
             }
-            std::cout << "\n";
-        std::cout << "Would you like to play again?\n";
-        std::cout << "\n";
-        std::cout << "0) Yes\n";
-        std::cout << "9) No\n";
-        std::cout << "Response: ";
-        std::cin >> user;
-        if (user == 9) {
-            exit = true;
-            std::cout << "\n";
-            std::cout << "Goodbye!!\n";
         }
-        else {
+        valid = false;
+        while (valid == false) {
             std::cout << "\n";
-            std::cout << " Please enter a valid input!\n";
+            std::cout << "Would you like to play again?\n";
+            std::cout << "\n";
+            std::cout << "Response: ";
+            std::cin >> input_string;
+            std::cout << "\n";
+            if (input_string == "no" || input_string == "No") {
+                exit = true;
+                std::cout << "\n";
+                std::cout << "Goodbye!!\n";
+                valid = true;
             }
+            else if (input_string == "yes" || input_string == "Yes") {
+                valid = true;
+            }
+            else {
+                std::cout << "\n";
+                std::cout << " Please enter a valid input!\n";
+                valid = false;
+                }
+        }
     }
 
 }
